b09d3c7126daed15f2675876425f8a1f0f769aac
OCH <dkdldy2@naver.com>
2024-04-14 19:59:10 +0900

    스프링부트 공부 3일차 커밋!

--- hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,19 +3,18 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
 
     public Long join(Member member) {
 
