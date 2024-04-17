62841962b850ef87314a830e13235943330418f1
OCH <dkdldy2@naver.com>
2024-04-17 15:55:06 +0900

    스프링 부트 7일차 - AOP 커밋

--- hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
@@ -32,5 +33,10 @@ public class SpringConfig {
 //        return new JdbcMemberRepository(dataSource);
 //          return new JpaMemberRepository(em);
 
+//    }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
 //    }
 }
